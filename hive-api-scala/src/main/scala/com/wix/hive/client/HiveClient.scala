575964899addf062b09432cb42aaa7f3d0aac3f0
Maxim Novak <maximn@wix.com>
2014-10-20 19:33:06 +0300

    Hive client refactoring

--- hive-api-scala/src/main/scala/com/wix/hive/client/HiveClient.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/client/HiveClient.scala
@@ -13,12 +13,16 @@ private object DefaultHttpClientFactory {
   def create: AsyncHttpClient = new DispatchHttpClient(Configuration.baseUrl)
 }
 
-class HiveClient(val appId: String, val secretKey: String, val instanceId: String, httpClient: AsyncHttpClient = DefaultHttpClientFactory.create) {
+private object DefaultConfigurations{
+  val baseUrl = "https://openapi.wix.com/v1/"
+}
+
+class HiveClient(val appId: String, val secretKey: String, val instanceId: String,
+                 httpClient: AsyncHttpClient = DefaultHttpClientFactory.create,
+                  val baseUrl: String = DefaultConfigurations.baseUrl) {
   def timestamp: String = new DateTime().toString(ISODateTimeFormat.dateTime())
 
-  val version = "1.0"
-
-  val baseUrl = "https://openapi.wix.com/v1"
+  val version = "1.0.0"
 
   val agent = s"Hive Scala v$version"
 
@@ -32,20 +36,25 @@ class HiveClient(val appId: String, val secretKey: String, val instanceId: Strin
 
 
   def execute[TCommandResult <: HiveBaseCommandResponse : ClassTag](command: HiveBaseCommand[TCommandResult]): Future[TCommandResult] = {
-    val httpData = command.createHttpRequestData
+    val httpDataFromCommand = command.createHttpRequestData
 
-    val httpDataWithClientInfo = httpData.copy(
+    val httpDataForRequest = (withClientData _ andThen withSignature _)(httpDataFromCommand)
+
+    httpClient.request(httpDataForRequest)
+  }
+
+  def withSignature(httpData: HttpRequestData): HttpRequestData = {
+    val signature = HiveSigner.getSignature(secretKey, httpData)
+    httpData.copy(headers = httpData.headers + (HeaderKeys.signature -> signature))
+  }
+
+  def withClientData(httpData: HttpRequestData): HttpRequestData = {
+    httpData.copy(
       url = baseUrl + httpData.url,
       headers = httpData.headers +
         (HeaderKeys.instanceId -> instanceId) +
         (HeaderKeys.applicationId -> appId) +
         (HeaderKeys.timestamp -> timestamp) +
         (HeaderKeys.userAgent -> agent))
-
-    val signature = HiveSigner.getSignature(secretKey, httpData)
-
-    val httpDataWithSignature = httpDataWithClientInfo.copy(headers =  httpDataWithClientInfo.headers + (HeaderKeys.signature -> signature))
-
-    httpClient.request(httpDataWithSignature)
   }
 }
