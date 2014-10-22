161ae1553f3759246f61abb62776eb7471082968
Maxim Novak <maximn@wix.com>
2014-10-22 14:45:27 +0300

    integration test with simplicator

--- hive-api-tests/src/it/scala/com/wix/hive/client/HiveClientWithSimplicatorIT.scala
+++ hive-api-tests/src/it/scala/com/wix/hive/client/HiveClientWithSimplicatorIT.scala
@@ -16,8 +16,6 @@ import org.apache.log4j.BasicConfigurator
  * Date: 10/20/14
  */
 class HiveClientWithSimplicatorIT extends BaseHiveClientIT with HubSimplicator {
-  skipAll
-
   val serverPort = 8089
 
   override val baseUrl = s"http://localhost:$serverPort"
@@ -35,24 +33,17 @@ class HiveClientWithSimplicatorIT extends BaseHiveClientIT with HubSimplicator {
 trait HubSimplicator extends HiveApiDrivers {
   val mapper = new ObjectMapper().registerModules(DefaultScalaModule, new JodaModule)
 
-  def versionedUrlMatcher(url: String) = urlMatching(s"v1$url")
+  def versionedUrlMatcher(url: String) = urlMatching(s"/v1$url")
 
   override def givenContactFetchById(myself: AppDef, respondsWith: Contact): Unit = {
     val contactJson = mapper.writeValueAsString(respondsWith)
+    val base64Regex = "[A-Za-z0-9+/_-]*"
 
-    givenThat(get(urlMatching("/contacts/.*")).
-      //withHeader("x-wix-application-id", equalTo(myself.appId)).
-      //withHeader("x-wix-instance-id", equalTo(myself.instanceId)).
-      //withHeader("x-wix-timestamp", matching("/^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?$/")).
-      //withHeader("x-wix-signature", matching("*")).
+    givenThat(get(versionedUrlMatcher("/contacts/.*")).
+      withHeader("x-wix-application-id", equalTo(myself.appId)).
+      withHeader("x-wix-instance-id", equalTo(myself.instanceId)).
+      withHeader("x-wix-timestamp", matching(".*")).
+      withHeader("x-wix-signature", matching(base64Regex)).
       willReturn(aResponse().withBody(contactJson).withStatus(200)))
-
-    //    import myself._
-    //
-    //    val smId = randomGuid[SMCollection]
-    //    givenAppsDefined(myself)
-    //    givenMetaSiteWith(smId, appId.asGuid[WixApp], instanceId.asId[WixAppInstance])
-    //    givenMetaSiteWith(ContactsReadPersonal)(appId.asGuid[WixApp], instanceId.asId[WixAppInstance])
-    //    givenContactServerWith(smId, respondsWith.id.asGuid[ContactDto])(contact.copy(id = respondsWith.id))
   }
 }
