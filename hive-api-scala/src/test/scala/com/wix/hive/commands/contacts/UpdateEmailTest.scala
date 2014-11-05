3885823572923effa2e8a26f7ac2dc5a95c46842
Maxim Novak <maximn@wix.com>
2014-11-05 17:11:33 +0200

    update email

--- hive-api-scala/src/test/scala/com/wix/hive/commands/contacts/UpdateEmailTest.scala
+++ hive-api-scala/src/test/scala/com/wix/hive/commands/contacts/UpdateEmailTest.scala
@@ -6,6 +6,14 @@ import com.wix.hive.model.contacts.EmailStatus
 import org.specs2.mutable.{SpecificationWithJUnit, Specification}
 
 class UpdateEmailTest extends SpecificationWithJUnit with HiveMatchers {
+
+  class Context extends ContextForModification {
+    val emailId = "222163d7-7d97-49aa-8ee2-e1af48db4241"
+    val email = ContactEmailDTO("tag-address", "some@wix.com", EmailStatus.OptOut)
+
+    val cmd = UpdateEmail(contactId, modifiedAt, emailId, email)
+  }
+
   "createHttpRequestData" should {
     "work with parameters" in new Context {
       cmd.createHttpRequestData must httpRequestDataWith(
@@ -16,12 +24,4 @@ class UpdateEmailTest extends SpecificationWithJUnit with HiveMatchers {
       )
     }
   }
-
-  class Context extends ContextForModification {
-    val emailId = "222163d7-7d97-49aa-8ee2-e1af48db4241"
-    val email = ContactEmailDTO("tag-address", "some@wix.com", EmailStatus.OptOut)
-
-    val cmd = UpdateEmail(contactId, modifiedAt, emailId, email)
-  }
-
 }
