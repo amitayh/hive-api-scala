e607b43517410833c147ca8a9c37d3c6d5525e4e
Maxim Novak <maximn@wix.com>
2014-12-03 10:44:12 +0200

    fixed address for PostActivity(ContactInfo)

--- hive-api-scala/src/main/scala/com/wix/hive/model/activities/ContactActivity.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/model/activities/ContactActivity.scala
@@ -4,4 +4,4 @@ import com.wix.hive.model.contacts._
 
 case class ContactActivity(name: Option[ContactName] = None, picture: Option[String] = None, company: Option[ActivityCompany] = None,
                            emails: Seq[ActivityContactEmail] = Nil, phones: Seq[ActivityContactPhone] = Nil,
-                           addresses: Seq[Address] = Nil, dates: Seq[ActivityImportantDate] = Nil, urls: Seq[ActivityContactUrl] = Nil)
+                           addresses: Seq[ActivityAddress] = Nil, dates: Seq[ActivityImportantDate] = Nil, urls: Seq[ActivityContactUrl] = Nil)
--- hive-api-scala/src/main/scala/com/wix/hive/model/contacts/Address.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/model/contacts/Address.scala
@@ -2,3 +2,11 @@ package com.wix.hive.model.contacts
 
 case class Address(tag: String, id: Option[Int] = None, address: Option[String] = None, neighborhood: Option[String] = None, city: Option[String] = None, region: Option[String] = None,
                    country: Option[String] = None, postalCode: Option[String] = None)
+
+case class ActivityAddress(tag: String,
+                   address: Option[String],
+                   neighborhood: Option[String],
+                   city: Option[String],
+                   region: Option[String],
+                   postalCode: Option[String],
+                   country: Option[String])
