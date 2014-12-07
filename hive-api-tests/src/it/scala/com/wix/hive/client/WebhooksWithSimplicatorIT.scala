6081e55a143c21dd728154635ee4df5a54df1d8d
Maxim Novak <maximn@wix.com>
2014-12-07 16:06:26 +0200

    rename - added IT to test filename

--- hive-api-tests/src/it/scala/com/wix/hive/client/WebhooksWithSimplicatorIT.scala
+++ hive-api-tests/src/it/scala/com/wix/hive/client/WebhooksWithSimplicatorIT.scala
@@ -6,6 +6,6 @@ import com.wix.hive.client.infrastructure.SimplicatorWebhooksDriver
  * User: maximn
  * Date: 12/2/14
  */
-class WebhooksWithSimplicator extends BaseWebhooksIT with SimplicatorWebhooksDriver {
+class WebhooksWithSimplicatorIT extends BaseWebhooksIT with SimplicatorWebhooksDriver {
 
 }
