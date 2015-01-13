280cf1d1a7727e79c62541cd6c8b66d1ded05944
Maxim Novak <maximn@wix.com>
2015-01-13 21:14:20 +0200

    fixed optional finagle on tests module

--- hive-api-scala/src/main/scala/com/wix/hive/server/webhooks/WebhooksConverter.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/server/webhooks/WebhooksConverter.scala
@@ -20,7 +20,7 @@ trait  WebhooksConverter extends HttpRequestHelpers {
   private lazy val validator = new WebhookSignatureVerification(secret)
   private lazy val marshaller = new WebhooksMarshaller
 
-  def convert[T <% HttpRequestData](req: T): Try[Webhook[_]] = {
+  def convert[T <% HttpRequestData](req: T): Try[Webhook[_ <: WebhookData]] = {
     val tryHeaderForReq = tryHeader(req, _: String)
 
     for {
