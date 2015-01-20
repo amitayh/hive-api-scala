558f5635dd4d7570e5a1b9c4226b78db0d9e18e0
Maxim Novak <maximn@wix.com>
2015-01-20 18:47:11 +0200

    Jackson -Ignore unknown properties - to support backward/forward compatibility

--- hive-api-scala/src/main/scala/com/wix/hive/json/JacksonObjectMapper.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/json/JacksonObjectMapper.scala
@@ -1,11 +1,11 @@
 package com.wix.hive.json
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.databind.SerializationFeature._
 import com.fasterxml.jackson.datatype.joda.JodaModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 object JacksonObjectMapper {
   lazy val mapper = new ObjectMapper().registerModules(DefaultScalaModule, new JodaModule())
-    .disable(WRITE_DATES_AS_TIMESTAMPS)
+    .disable(WRITE_DATES_AS_TIMESTAMPS).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 }
--- hive-api-scala/src/main/scala/com/wix/hive/model/contacts/ContactEmail.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/model/contacts/ContactEmail.scala
@@ -1,6 +1,7 @@
 package com.wix.hive.model.contacts
 
+import com.fasterxml.jackson.module.scala.JsonScalaEnumeration
 import com.wix.hive.model.contacts.EmailStatus._
 
-case class ContactEmail(tag: String, email: String, emailStatus: EmailStatus, id: Option[Int] = None)
+case class ContactEmail(tag: String, email: String, @JsonScalaEnumeration(classOf[EmailStatusRef]) emailStatus: EmailStatus, id: Option[Int] = None)
 case class ActivityContactEmail(tag: String, email: String)
