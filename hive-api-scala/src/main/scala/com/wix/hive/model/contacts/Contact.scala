d1d2bd74c68a1b4c346a2e09b33c7257394c186f
Maxim Novak <maximn@wix.com>
2015-01-21 16:42:03 +0200

    modifiedAt - changed to DateTime

--- hive-api-scala/src/main/scala/com/wix/hive/model/contacts/Contact.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/model/contacts/Contact.scala
@@ -1,7 +1,5 @@
 package com.wix.hive.model.contacts
 
-import java.util.Date
-
 import org.joda.time.DateTime
 
 case class Contact(id: String, createdAt: DateTime, name: Option[ContactName] = None,
@@ -9,4 +7,4 @@ case class Contact(id: String, createdAt: DateTime, name: Option[ContactName] = 
                    tags: Seq[String] = Nil, emails: Seq[ContactEmail] = Nil, phones: Seq[ContactPhone] = Nil,
                    addresses: Seq[Address] = Nil, urls: Seq[ContactUrl] = Nil, dates: Seq[ImportantDate] = Nil,
                    notes: Seq[Note] = Nil, custom: Seq[CustomField] = Nil, links: Seq[StateLink] = Nil,
-                   modifiedAt: Option[Date] = None)
+                   modifiedAt: Option[DateTime] = None)
