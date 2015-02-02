d927a0f1c28cba3eea5a168fa2e51f9ea9fa67cd
Karen Cohen <karenc@wix.com>
2015-02-02 14:55:36 +0200

    LabelType enum will now be serialized correctly

--- hive-api-scala/src/main/scala/com/wix/hive/model/labels/LabelTypes.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/model/labels/LabelTypes.scala
@@ -1,5 +1,9 @@
 package com.wix.hive.model.labels
 
+import com.fasterxml.jackson.core.`type`.TypeReference
+
+class LabelTypesRef extends TypeReference[LabelTypes.type]
+
 object LabelTypes extends Enumeration {
   type LabelType = Value
   val `system` = Value("system")
