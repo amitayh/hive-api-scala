34ebf98347644fdd198a9b2790157cdc00874c2e
Maxim Novak <maximn@wix.com>
2015-02-24 14:50:31 +0200

    added emailSubscriptionPolicy

--- hive-api-scala/src/main/scala/com/wix/hive/model/activities/ContactActivity.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/model/activities/ContactActivity.scala
@@ -1,7 +1,17 @@
 package com.wix.hive.model.activities
 
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.module.scala.JsonScalaEnumeration
+import com.wix.hive.model.activities.EmailSubscriptionPolicy.EmailSubscriptionPolicy
 import com.wix.hive.model.contacts._
 
 case class ContactActivity(name: Option[ContactName] = None, picture: Option[String] = None, company: Option[ActivityCompany] = None,
                            emails: Seq[ActivityContactEmail] = Nil, phones: Seq[ActivityContactPhone] = Nil,
-                           addresses: Seq[ActivityAddress] = Nil, dates: Seq[ActivityImportantDate] = Nil, urls: Seq[ActivityContactUrl] = Nil)
+                           addresses: Seq[ActivityAddress] = Nil, dates: Seq[ActivityImportantDate] = Nil, urls: Seq[ActivityContactUrl] = Nil, @JsonScalaEnumeration(classOf[EmailSubscriptionPolicyRef])emailSubscriptionPolicy: Option[EmailSubscriptionPolicy] = None)
+
+class EmailSubscriptionPolicyRef extends TypeReference[EmailSubscriptionPolicy.type]
+
+object EmailSubscriptionPolicy extends Enumeration {
+  type EmailSubscriptionPolicy = Value
+  val Recurring = Value("RECURRING")
+}
