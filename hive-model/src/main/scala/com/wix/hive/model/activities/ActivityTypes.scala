8c3f56482c1609cc69a7b6defec24a5cc58af9c7
Simonas Pinevičius <simonasp@wix.com>
2015-05-12 16:59:48 +0300

    Replace ActivityTypes @JsonCreator with custom deserializer

--- hive-model/src/main/scala/com/wix/hive/model/activities/ActivityTypes.scala
+++ hive-model/src/main/scala/com/wix/hive/model/activities/ActivityTypes.scala
@@ -1,18 +1,29 @@
 package com.wix.hive.model.activities
 
-import com.fasterxml.jackson.annotation.{JsonCreator, JsonProperty}
+import com.fasterxml.jackson.core.{JsonParser, ObjectCodec}
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.{DeserializationContext, JsonDeserializer}
 import com.wix.hive.model.activities.ActivityType.ActivityType
 import com.wix.hive.model.activities.ActivityTypes.knownNames
 
-case class ActivityTypes(types: Seq[ActivityType]) {
-
-  @JsonCreator
-  def this(@JsonProperty("types") types: Seq[String], @JsonProperty("hack_to_overcome_type_erasure") ignored: Option[_] = None) = this(
-    types.filter(knownNames).map(name => ActivityType.withName(name))
-  )
-
-}
+@JsonDeserialize(using = classOf[ActivityTypesDeserializer])
+case class ActivityTypes(types: Seq[ActivityType])
 
 object ActivityTypes {
   val knownNames = ActivityType.values.map(_.toString)
+}
+
+private class ActivityTypesDeserializer extends JsonDeserializer[ActivityTypes] {
+  override def deserialize(json: JsonParser, context: DeserializationContext): ActivityTypes = {
+    import scala.collection.JavaConverters._
+
+    val codec: ObjectCodec = json.getCodec
+    val node: ObjectNode = codec.readTree(json)
+
+    ActivityTypes(
+      node.withArray("types").asScala.map(_.asText).filter(knownNames).map(name => ActivityType.withName(name)).toSeq
+    )
+
+  }
 }
