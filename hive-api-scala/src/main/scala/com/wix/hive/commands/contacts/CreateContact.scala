1c755ecee68bd4a7c8c67e9fbd7a38778607a910
Karen Cohen <karenc@wix.com>
2015-06-08 12:30:06 +0300

    CreateContact with custom fields

--- hive-api-scala/src/main/scala/com/wix/hive/commands/contacts/CreateContact.scala
+++ hive-api-scala/src/main/scala/com/wix/hive/commands/contacts/CreateContact.scala
@@ -5,14 +5,14 @@ import com.wix.hive.model.contacts.{Company, ContactName, _}
 
 case class CreateContact(name: Option[ContactName] = None, picture: Option[String] = None, company: Option[Company] = None,
                          emails: Seq[ContactEmailDTO] = Nil, phones: Seq[ContactPhoneDTO] = Nil,
-                         addresses: Seq[ContactAddress] = Nil, urls: Seq[ContactUrl] = Nil, dates: Seq[ImportantDate] = Nil)
+                         addresses: Seq[ContactAddress] = Nil, urls: Seq[ContactUrl] = Nil, dates: Seq[ImportantDate] = Nil, custom: Seq[CustomField]= Nil)
   extends ContactsCommand[CreatedContact] {
   override val method = HttpMethod.POST
 
-  override val body = Some(ContactData(name, picture, company, emails, phones, addresses, urls, dates))
+  override val body = Some(ContactData(name, picture, company, emails, phones, addresses, urls, dates, custom))
 }
 
 
 case class ContactData(name: Option[ContactName] = None, picture: Option[String] = None, company: Option[Company] = None,
                        emails: Seq[ContactEmailDTO] = Nil, phones: Seq[ContactPhoneDTO] = Nil,
-                       addresses: Seq[ContactAddress] = Nil, urls: Seq[ContactUrl] = Nil, dates: Seq[ImportantDate] = Nil)
+                       addresses: Seq[ContactAddress] = Nil, urls: Seq[ContactUrl] = Nil, dates: Seq[ImportantDate] = Nil, custom: Seq[CustomField]= Nil)
