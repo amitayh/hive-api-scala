0172a5b08103e03b50e68908109a3f3d7eb7093d
Simonas Pinevičius <simonasp@wix.com>
2015-06-22 16:56:29 +0300

    Added SiteSettings dto

---
+++ hive-model/src/main/scala/com/wix/hive/model/sites/SiteSettings.scala
@@ -0,0 +1,28 @@
+package com.wix.hive.model.sites
+
+case class SiteSettings(locale: Locale = Locale(),
+                        locationInfo: Seq[LocationInfo] = Nil)
+
+case class Locale(language: Option[String] = None,
+                  currency: Option[String] = None,
+                  dateFormat: Option[String] = None,
+                  startWeek: Option[String] = None,
+                  timeZone: Option[String] = None)
+
+case class LocationInfo(name: Option[String] = None,
+                        description: Option[String] = None,
+                        email: Option[String] = None,
+                        phone: Option[String] = None,
+                        fax: Option[String] = None,
+                        address: Option[Address] = None,
+                        accessibility: Option[Boolean] = None,
+                        parking: Option[Boolean] = None)
+
+case class Address(street: Option[String] = None,
+                   city: Option[String] = None,
+                   country: Option[String] = None,
+                   state: Option[String] = None,
+                   zip: Option[String] = None,
+                   geoLocation: Option[GeoLocation] = None)
+
+case class GeoLocation(lat: Double, long: Double)
