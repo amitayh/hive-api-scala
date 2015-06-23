cce79b503a597c2e095c9f8dea492d85c1246097
Simonas Pinevičius <simonasp@wix.com>
2015-06-23 16:34:15 +0300

    Completed SiteSettings dto

--- hive-model/src/main/scala/com/wix/hive/model/sites/SiteSettings.scala
+++ hive-model/src/main/scala/com/wix/hive/model/sites/SiteSettings.scala
@@ -1,7 +1,30 @@
 package com.wix.hive.model.sites
 
-case class SiteSettings(locale: Locale = Locale(),
-                        locationInfo: Seq[LocationInfo] = Nil)
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.joda.time.DateTime
+
+case class SiteSettings(title: String,
+                        publishStatus: PublishStatus = PublishStatus(isPublished = false),
+                        siteMembersAutoApprove: Boolean = false,
+                        disableCookies: Boolean = false,
+                        indexableBySearchEngines: Boolean = false,
+                        mobileOptimized: Boolean = false,
+                        contactFormEmail: Option[String] = None,
+                        @JsonProperty("package") packagePurchased: Option[String] = None,
+                        mailbox: Option[String] = None,
+                        favIcon: Option[String] = None,
+                        logo: Option[Logo] = None,
+                        domainInfo: Seq[DomainInfo] = Nil,
+                        locale: Option[Locale] = None,
+                        locationInfo: Seq[LocationInfo] = Nil,
+                        category: Option[SiteCategory] = None,
+                        goals: Seq[String] = Nil)
+
+case class PublishStatus(isPublished: Boolean, lastPublishDate: Option[DateTime] = None)
+
+case class Logo(logo: Option[String] = None, squareLogo: Option[String] = None)
+
+case class DomainInfo(domain: String, googleAnalyticsId: Option[String] = None, isPrimary: Boolean)
 
 case class Locale(language: Option[String] = None,
                   currency: Option[String] = None,
@@ -15,7 +38,9 @@ case class LocationInfo(name: Option[String] = None,
                         fax: Option[String] = None,
                         address: Option[Address] = None,
                         accessibility: Option[Boolean] = None,
-                        parking: Option[Boolean] = None)
+                        parking: Option[Boolean] = None,
+                        paymentMethods: Seq[String] = Nil,
+                        workingHours: Seq[WorkDay] = Nil)
 
 case class Address(description: Option[String] = None,
                    street: Option[String] = None,
@@ -26,3 +51,9 @@ case class Address(description: Option[String] = None,
                    geoLocation: Option[GeoLocation] = None)
 
 case class GeoLocation(lat: Double, long: Double)
+
+case class SiteCategory(primary: Option[String] = None, secondary: Option[String] = None)
+
+case class WorkDay(dayOfWeek: Option[String] = None, timeFrames: Seq[TimeFrame] = Nil)
+
+case class TimeFrame(start: DateTime, end: DateTime)
