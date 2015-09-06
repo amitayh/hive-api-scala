a1cbe770d3d25da15cf619b889f99070d8bf3a9f
Maxim Novak <maximn@wix.com>
2015-09-06 16:03:02 +0300

    isHidden field for pages info api

--- hive-model/src/main/scala/com/wix/hive/model/sites/Page.scala
+++ hive-model/src/main/scala/com/wix/hive/model/sites/Page.scala
@@ -4,4 +4,4 @@ package com.wix.hive.model.sites
  * @author maximn
  * @since 06-Sep-2015
  */
-case class Page(path: String, wixPageId: Option[String], appPageId: Option[String], title: String)
+case class Page(path: String, wixPageId: Option[String], appPageId: Option[String], title: String, isHidden: Boolean)
